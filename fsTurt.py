a1363e6c50cd0af4af3d9c1b14972e19c38a58c3
FOSSBOSS <blicken@gmail.com>
2024-03-18 20:09:39 -0400

    Update fsTurt.py

--- fsTurt.py
+++ fsTurt.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import turtle
-# Discount Sketchy Etch version for Lame standard keyboard hardware
-# 
+import os
+# Discount Sketch-Y-Etch version for standard keyboard hardware
+# OK turns out Im super frustrated with raw_turtle  and GPIO latency.
+# using a teensy for IO, and processing the keypresses.
+
 t = turtle.Turtle()
 t.home()
+#t.shapesize(0.5)  # Cursor size: 0.5 is half of normal
+t.width(10)
 
 i = 0   #index: current colour count
 
@@ -12,23 +17,25 @@ def move(key):
     y = t.ycor()
     screen.title(f"X: {t.xcor()}, Y: {t.ycor()}")
     if key == "Up":
-        y += 1
+        y += 10
         t.goto(x, y )
 
     elif key == "Down":
-        y -= 1
+        y -= 10
         t.goto(x, y )
 
     elif key == "Left":
-        x -= 1
+        x -= 10
         t.goto(x, y)
 
     elif key == "Right":
-        x += 1
+        x += 10
         t.goto(x, y)
 
     elif key == "Clear":    # c key
         t.reset()
+        #t.shapesize(0.5)  # Cursor size: 0.5 is half of normal
+        t.width(10)
         
     elif key == "Quit":      # q key
         exit()
@@ -40,9 +47,34 @@ def Colour(channel):     # g key
     t.pencolor(colors[i % len(colors)])  # Set pen color based on the current index of the length of the color list
     i += 1
 
+def lift_pen(channel):   # l key
+    if t.isdown():
+        t.penup()
+    else:
+        t.pendown()
+
+def saveSVG(channel):    # s key
+    # utilizing geg
+    # Get the current Unix time
+    current_time = int(time.time())
+    t.hideturtle()
+    canvas_data = turtle.getcanvas().postscript()
+    # Construct the file name with the current time and the .ps extension
+    ps_file_name = f"{current_time}.ps"
+    svg_file_name = f"{current_time}.svg"
+    File_path = '/home/e/img'
+    output_path = os.path.join(File_path, svg_file_name)
+    with open(ps_file_name, 'w') as f:
+        f.write(canvas_data)
+    # Show the turtle again
+    t.showturtle()
+    os.system('eps2svg ' + ps_file_name + ' ' + output_path)
+    os.system('rm ' + ps_file_name)
+
+#def demo(channel):
 
 screen = turtle.Screen()
-#screen.setup(width=1.0, height=1.0)  # Set the window to full-screen mode
+screen.setup(width=1.0, height=1.0)  # Set the window to full-screen mode
 
 canvas = screen.getcanvas()
 root = canvas.winfo_toplevel()
@@ -56,6 +88,14 @@ screen.onkeypress(lambda: move("Right"), "Right")
 screen.onkeypress(lambda: move("Clear"), "c")
 screen.onkeypress(lambda: move("Quit"), "q")
 screen.onkeypress(lambda: Colour("Color"), "g")
+screen.onkeypress(lambda: lift_pen("lift"), "l")
+screen.onkeypress(lambda: saveSVG("save"), "s")
+#screen.onkeypress(lambda: demo("demo"), "d")
+
+screen.listen()  # Listen for key presses
+
+screen.mainloop()
+
 
 screen.listen()  # Listen for key presses
 
